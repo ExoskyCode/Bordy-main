e567564dac5857a74919967588e65d0f852c5447
ExoskyCode <exoskyv2@gmail.com>
2023-03-04 14:59:37 +0100

    Board Show all Boards from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import { BoardCard } from '~/src/components/BoardCard/BoardCard';
+import { BoardCard } from '~/src/components/Board/BoardCard';
+import { Button } from '~/src/components/Board/CreateBoard/CreateBoardBtn';
 import { prisma } from '~/src/db/prisma';
 
 export default async function Home() {
 	const boards = await prisma.board.findMany();
 	return (
-		<div>
+		<div className='flex flex-col gap-8'>
+			<h1 className='text-4xl font-bold'>Boards List</h1>
+
+			<Button as='a' href='/boards/new' className='self-start'>
+				Create new Board
+			</Button>
+
 			<ul className='flex flex-col gap-4'>
 				{boards.map((board) => (
 					<BoardCard key={board.id} board={board} />
