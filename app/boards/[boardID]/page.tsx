1cf61537c86f0628af215e1a2f2c3ac15ef7a77a
ExoskyCode <exoskyv2@gmail.com>
2023-03-04 18:22:07 +0100

    update

--- app/boards/[boardID]/page.tsx
+++ app/boards/[boardID]/page.tsx
@@ -1,22 +1,58 @@
 import { notFound } from 'next/navigation';
+import { Proposition } from '~/src/components/Proposition/Proposition';
+import { PropositionForm } from '~/src/components/Proposition/PropositionForm';
 import { prisma } from '~/src/db/prisma';
 
-export default async function page({
+export default async function BoardPage({
 	params,
 }: {
 	params: { boardID: string };
 }) {
-	const boardID = Number(params.boardID);
+	const boardId: number = Number(params.boardID);
 
-	if (isNaN(boardID)) {
-		return notFound;
+	if (isNaN(boardId)) {
+		return notFound();
 	}
 
 	const board = await prisma.board.findUniqueOrThrow({
 		where: {
-			id: boardID,
+			id: boardId,
 		},
-	})
-	
-	return <h2>{board.title}</h2>
+	});
+
+	const propositions = await prisma.proposition.findMany({
+		where: {
+			boardId: boardId,
+		},
+		orderBy: {
+			vote: {
+				_count: 'desc',
+			},
+		},
+		select: {
+			title: true,
+			id: true,
+			_count: {
+				select: {
+					vote: true,
+				},
+			},
+		},
+	});
+
+	return (
+		<div className='flex flex-col gap-8'>
+			<h1 className='text-3xl font-bold'>{board.title}</h1>
+			<PropositionForm boardId={board.id} />
+			<ul className='flex flex-col gap-4'>
+				{propositions.map((proposition) => (
+					<Proposition
+						key={proposition.id}
+						voteCount={proposition._count.vote}
+						{...proposition}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
