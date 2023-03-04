81fc07b7f906da5f6022086bc6fd187493265fec
ExoskyCode <exoskyv2@gmail.com>
2023-03-04 14:59:43 +0100

    update

--- app/boards/[boardID]/page.tsx
+++ app/boards/[boardID]/page.tsx
@@ -1,3 +1,10 @@
-export default function page({ params }: { params: { boardID: string } }) {
+import { prisma } from '~/src/db/prisma';
+
+export default async function page({
+	params,
+}: {
+	params: { boardID: string };
+}) {
+	const board = await prisma.board.findUniqueOrThrow();
 	return <h1>Vous êtes au board ({params.boardID})</h1>;
 }
